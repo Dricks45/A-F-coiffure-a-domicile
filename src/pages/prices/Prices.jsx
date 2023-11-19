507ec03c77dde699f31e01fcc547b18ff52aa74c
Dricks45 <144217492+Dricks45@users.noreply.github.com>
2023-11-19 18:46:21 +0100

    version imprimable des tarifs

--- src/pages/prices/Prices.jsx
+++ src/pages/prices/Prices.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import "./prices.scss";
+import "./print.scss";
 
 function Prices() {
   const [data, setData] = useState([]);
@@ -19,18 +20,35 @@ function Prices() {
       });
   }, []);
 
+  const handleprint = () => {
+    window.print();
+  };
+
   return (
     <section className="outlet">
       <h2 className="title_prices">Liste des Tarifs</h2>
       <hr />
+
       <div className="container_prices">
+        <p className="paragraph_prices">Uniquement sur rendez-vous.</p>
         {data.map((item) => (
           <p className="paragraph_prices" key={item.id}>
             {item.prestation} = <br /> Court : {item.court}, Long : {item.long}
           </p>
         ))}
-        <p>Imprimer les tarifs</p>
+        <p className="paragraph_prices">
+          Frais de déplacement à partir de 20 km : 1 € / km.
+        </p>
+        <button className="print_prices" onClick={handleprint}>
+          Imprimer les tarifs
+        </button>
       </div>
+      <p className="group_rates">
+        Tarifs groupe : Rejoignez vous chez un(e) ami(e) et bénéficiez de 3 € de
+        remise par forfait à partir de 3 personnes &quot;hors tarifs
+        enfants&quot;.
+      </p>
+      <p className="text_print">TEL : 07.82.32.11.79</p>
     </section>
   );
 }
